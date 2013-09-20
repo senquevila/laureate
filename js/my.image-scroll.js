062a728efe923e44a60967857518df65195e1c1b
senquevila <senquevila@gmail.com>
2013-09-19 18:00:04 -0600

    Iniciando con el Scroll

--- js/my.image-scroll.js
+++ js/my.image-scroll.js
@@ -1,29 +1,34 @@
 (function ($) {
 	$.fn.ImageScroll = function (userOptions) {
-		var _defaults = {}
+		var _defaults = {};
 		var _options = $.extend(_defaults, userOptions);
 		var _this = $(this);
+		var _images = {};
 
 		this.each(function () {
 			init(_this);
 		});
 
 		function init(_this) {
-			var buttonUp = $('<a />');		
-			var buttonDown = $('<a />');
+			var buttonUp = $('<div />');		
+			var buttonDown = $('<div />');
 			var content = $('<div/>');
 
-			buttonUp.attr('href', '#up').attr('class', 'nav').append('UP');
-			buttonDown.attr('href', '#down').attr('class', 'nav').append('DOWN');
+			buttonUp.attr('id', 'up').attr('class', 'nav').append('UP');
+			buttonDown.attr('id', 'down').attr('class', 'nav').append('DOWN');
 
 			var images = _this.children('img');
 
 			images.each (function (i, e) {
-				var elem = $(e);
-				var item = $('<a />');
+				var elem = $(e);				
+				var item = $('<div />');
+				_images[i] = elem;
 				item.attr('href', elem.attr('src'))
+					.attr('class', 'scroll')
 					.append(elem.attr('title') + '<br>');
 
+				if (i >= _options.items)
+					item.attr('class', 'hide');
 
 				content.append(item);
 			});		
@@ -32,6 +37,10 @@
 			_this.append(buttonUp);			
 			_this.append(content);
 			_this.append(buttonDown);
+		}
+
+		function show(i) {
+
 		}
 
 	};
