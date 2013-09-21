4b1b803761f3c57795c6a1ca57ee732ae6f32242
senquevila <senquevila@gmail.com>
2013-09-21 02:35:29 -0600

    Trabajando en Scroll. Falta que cambia la imagen dinamicamente y que se adapta al tamanio del div, el cual debe estar a un lado de toda la animacion

--- js/my.image-scroll.js
+++ js/my.image-scroll.js
@@ -3,7 +3,8 @@
 		var _defaults = {};
 		var _options = $.extend(_defaults, userOptions);
 		var _this = $(this);
-		var _images = {};
+		var _actualImage = 0;
+		var _maxImage = 0;
 
 		this.each(function () {
 			init(_this);
@@ -13,44 +14,88 @@
 			var buttonUp = $('<div />');		
 			var buttonDown = $('<div />');
 			var content = $('<div/>');
+			var mainImage = $('<div/>')
 
-			buttonUp.attr('id', 'up').attr('class', 'nav').append('UP');
-			buttonDown.attr('id', 'down').attr('class', 'nav').append('DOWN');
+			buttonUp.attr('id', 'up')
+				.attr('class', 'nav')
+				.append('UP');
+			
+			buttonDown.attr('id', 'down')
+				.attr('class', 'nav')
+				.append('DOWN');
+
+			mainImage.attr('id', 'main-image');
+
+			_this.append(buttonUp);
 
 			var images = _this.children('img');
 
 			images.each (function (i, e) {
 				var elem = $(e);				
-				var item = $('<div />');
-				var boxThumb = $('<div />');
-				var imgThumb = $('img');
+				var boxThumb = $('<div />')
+				var textThumb = $('<div />')
+				var divThumb = $('<div />');			
 
-				_images[i] = elem;
+				divThumb.append(elem)
+					.attr('class', 'thumb');
 
-				item.attr('href', elem.attr('src'))
-					.attr('class', 'ImageScroll')
-					.append(elem.attr('title') + '<br>');
+				textThumb.append(elem.attr('title'))
+					.attr('class', 'text-thumb');
 
-				imgThumb.attr('src', elem.attr('src'));
+				boxThumb.append(divThumb)
+					.append(textThumb)
+					.attr('class', 'box-thumb')
+					.attr('id', 'scroll-' + i);
 
-				boxImg.attr('class', 'square-thumb')
-					.append(imgThumb);
+				if (i >= _options.items) {
+					boxThumb.attr('class', 'hide');
+				}
 
-				if (i >= _options.items)
-					item.attr('class', 'hide');
+				_this.append(boxThumb);
 
-				content.append(item);
-			});		
-
-			_this.text('');
-			_this.append(buttonUp);			
-			_this.append(content);
+				_maxImage++;
+				
+			});
+					
 			_this.append(buttonDown);
+			_this.append(mainImage);
 		}
 
-		function show(i) {
+		$('#up').on('click', function () {
+			if (_actualImage > 0) {
+				_actualImage--;
 
-		}
+				var first = _actualImage;
+				var last = _actualImage + _options.items;
+
+				$('#scroll-' + first).attr('class', 'box-thumb');
+				$('#scroll-' + last).attr('class', 'hide');
+			}
+		});
+
+		$('#down').on('click', function() {						
+			if (_actualImage + _options.items < _maxImage) {				
+				var first = _actualImage;
+				var last = _actualImage + _options.items;
+				
+				$('#scroll-' + first).attr('class', 'hide');
+				$('#scroll-' + last).attr('class', 'box-thumb');
+
+				_actualImage++;
+			}
+		});
+
+		$('.box-thumb').on('click', function() {
+			var id = $(this).attr('id');
+			var img = $('#' + id + ' > div > img');
+			var bigImage = $('<img/>');
+
+			bigImage.attr('src', img.attr('src'))
+				.attr('title', img.attr('title'))
+				.attr('alt', img.attr('alt'));
+
+			$('#main-image').append(bigImage);
+		});
 
 	};
 }(jQuery));
